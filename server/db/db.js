1fbb8d44eb369dfaec76623328af53aa2501a9ce
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-04-14 17:52:15 -0400

    m

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,14 @@
 //exports the Sequelize constructor
-const Sequelize = require('sequelize');
-
+const Sequelize = require("sequelize");
 
 const config = {
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 };
 
 if (process.env.LOGGING) {
@@ -11,6 +16,9 @@ if (process.env.LOGGING) {
 }
 
 //create an instance of sequelize
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/users', config);
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/users",
+  config
+);
 
 module.exports = db;
