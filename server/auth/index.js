10648079f7472388125306c91653a6167d2f7332
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-04-14 18:02:14 -0400

    Hi

--- server/auth/index.js
+++ server/auth/index.js
@@ -26,8 +26,7 @@ router.post('/signup', async (req, res, next) => {
 
 router.get('/me', async (req, res, next) => {
   try {
-    res.send()
-    // res.send(await User.findByToken(req.headers.authorization))
+    res.send(await User.findByToken(req.headers.authorization))
   } catch (ex) {
     next(ex)
   }
